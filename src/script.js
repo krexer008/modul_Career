6754a6494d834e618d39cd61f9a680b9a18d6cfb
krexer008 <y.fedor86@mail.ru>
2025-07-14 16:00:42 +0300

    feat(core): adding form template in process

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 "use strict";
 
+let currentVacancyId = null;
+
 window.addEventListener("beforeunload", () => {
   clearAllForms();
 });
-
-let currentVacancyId = null;
 
 const vacancies = [
   {
@@ -84,14 +84,15 @@ const vacancies = [
   },
 ];
 
-const vacancies_cards = document.querySelector(".vacancies-cards");
+// Vacancy cards
 
+const vacancies_cards = document.querySelector(".vacancies-cards");
 vacancies_cards.innerHTML = "";
 vacancies.forEach((vacancy) => {
   vacancies_cards.innerHTML += `
   <div class="vacancy-card">
           <div class="icon-vacancies-container">
-            <img class="icon-vacancies" src="modul_Career/src/assets/images/icons/Code.svg" alt="" />
+            <img class="icon-vacancies" src="modul_Career/src/assets/images/icons/Code.svg" alt="">
           </div>
           <div class="vacancy-position-container">
             <h4 class="vacancy-position">${vacancy.title}</h4>
@@ -103,6 +104,62 @@ vacancies.forEach((vacancy) => {
         </div>
   `;
 });
+
+// Form element template
+
+const formContainer = document.querySelector(".form-container");
+formContainer.innerHTML = "";
+
+const telPatern = "[\\+]\\d{1,3}\\s?[\\(\\d{1,3}\\)]?\\s?\\d{3}[\\s-]?\\d{2}[\\s-]?\\d{2}";
+
+const formTemplate = `
+<form id="dialogForm" class="career-form" method="post">
+    <div class="form-row">
+        <div class="form-group">
+            <label for="firstname" class="form-label">*Имя</label>
+            <input class="form-input" type="text" name="firstname" id="firstname" autocomplete="off" placeholder="Иван"
+                required>
+        </div>
+        <div class="form-group">
+            <label for="lastname" class="form-label">*Фамилия</label>
+            <input class="form-input" type="text" name="lastname" id="lastname" autocomplete="off" placeholder="Иванов"
+                required>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="tel" class="form-label">*Номер телефона</label>
+        <input class="form-input" type="tel" name="phone" id="tel" autocomplete="off" placeholder="+7(999)999 99 99"
+            pattern=${telPatern} required>
+    </div>
+    <div class="form-group">
+        <label for="email" class="form-label">*Почта</label>
+        <input class="form-input" type="email" name="email" id="email" autocomplete="off"
+            placeholder="ivnovovan@mail.ru" required>
+    </div>
+    <div class="form-group form-group_upload">
+        <div class="form-group">
+            <label for="url" class="form-label">*Ссылка на резюме или файл(pdf/doc)</label>
+            <input class="form-input" type="url" id="url" name="url" autocomplete="off" placeholder="ссылка на резюме"
+                size="50" required>
+        </div>
+        <button id="uploadDialogResume" class="upload-button">
+            <img class="icon-upload" src="src/assets/images/icons/UploadSimple.svg" alt="">
+            <span>загрузить файл</span>
+        </button>
+    </div>
+    <button class="form-button" type="submit" id="dialogFormSubmit">Отправить</button>
+</form>
+`;
+
+if (currentVacancyId) {
+  formContainer.innerHTML += `<div class="modal-form-title">
+                <h2 id="dialog-title" class="modal-title">Отклик на вакансию<br>${vacancy.title}</h2>
+                <button type="button" class="modal-close" id="closeForm">
+                  <img src="src/assets/images/icons/Menu.svg" alt="">
+                </button>
+              </div>`;
+  formContainer.innerHTML += formTemplate;
+}
 
 // Dialogs elements
 const formDialogWrapper = document.getElementById("formDialogWrapper");
@@ -124,7 +181,7 @@ const customForm = document.getElementById("customForm");
 const customFormSubmit = document.getElementById("customFormSubmit"); // Кнопка отправки
 
 // Opening form dialog
-if (formDialogWrapper && formOpenBtn) {
+if (formDialogWrapper && formOpenBtn && currentVacancyId) {
   formOpenBtn.addEventListener("click", (e) => {
     e.preventDefault();
     formDialogWrapper.showModal();
@@ -146,7 +203,7 @@ if (formDialogWrapper && formOpenBtn && formCloseBtn) {
 }
 
 // Dialog form handler
-if (formDialogWrapper && dialogForm && dialogFormSubmit && uploadDialogResume) {
+if (dialogForm && dialogFormSubmit && uploadDialogResume) {
   addInputListenersToForm(dialogForm);
 
   dialogFormSubmit.addEventListener("click", (e) => {
@@ -166,7 +223,9 @@ if (formDialogWrapper && dialogForm && dialogFormSubmit && uploadDialogResume) {
       // Добавить проверку пользователя
       //if (checkUser()) {
       //resumeSending();
-      formDialogWrapper.close();
+      if (currentVacancyId) {
+        formDialogWrapper.close();
+      }
       greetingsDialog.showModal();
       clearForm(dialogForm);
       //} else {
